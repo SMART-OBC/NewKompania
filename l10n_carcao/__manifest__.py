49f82b69ef7786bef0a0b6f2d1a89e34506a6216
zohaibrehman <zohaibrehman1991@gmail.com>
2021-12-08 23:10:44 +0500

    [Update] log info

---
+++ l10n_carcao/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+#    Module programed and financed by:
+#    Vauxoo, C.A. (<http://vauxoo.com>).
+#    Our Community team mantain this module:
+#    https://launchpad.net/~openerp-venezuela
+
+{
+    'name' : 'Curacao - Accounting',
+    'author': ['SMART'],
+    'category': 'Accounting/Localizations/Account Charts',
+    'description':
+"""
+Chart of Account for Curacao.
+===============================
+
+
+""",
+    'depends': ['account',
+    ],
+    'data': [
+             'data/l10n_ve_chart_data.xml',
+             'data/account.account.template.csv',
+             'data/l10n_ve_chart_post_data.xml',
+             # 'data/account_data.xml',
+             # 'data/account_tax_data.xml',
+             # 'data/account_chart_template_data.xml'
+    ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+}
