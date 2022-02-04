753348558db1f64ccd8ff5a7d3bf6afad78ac241
zohaibrehman <zohaibrehman1991@gmail.com>
2022-02-04 12:32:48 +0500

    [Update]

---
+++ rental_ecommerce/models/__init__.py
@@ -0,0 +1,3 @@
+from . import product
+from . import rental_wizard
+from . import sale_order_line
---
+++ rental_ecommerce/static/src/js/tours/rental_ecommerce_shop_backend.js
@@ -0,0 +1,8 @@
+odoo.define("rental_ecommerce.tour_shop_backend", function (require) {
+"use strict";
+
+var tour = require("web_tour.tour");
+var steps = require("rental_ecommerce.tour_shop");
+tour.register("shop", {url: "/rent"}, steps);
+
+});
