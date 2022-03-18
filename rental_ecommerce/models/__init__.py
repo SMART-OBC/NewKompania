96281778ef6efd06b54664e0909ac971e37923da
SMART-OBC <86059790+SMART-OBC@users.noreply.github.com>
2022-03-18 15:47:05 -0400

    Install Sign and remove everything else

--- rental_ecommerce/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import product
-from . import rental_wizard
-from . import sale_order_line
--- website_rentals/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import currency
-from . import product
-from . import rental_pricing
-from . import sale_order
